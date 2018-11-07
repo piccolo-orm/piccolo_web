788849651c478aceb360c473ea4d0712325250dc
Daniel Townsend <dan@dantownsend.co.uk>
2018-11-07 08:02:51 +0000

    added a second blog article

--- image/app/src/views/BlogList.vue
+++ image/app/src/views/BlogList.vue
@@ -19,6 +19,11 @@ export default {
                     title: 'Is Async Worthwhile?',
                     src: 'is_async_worthwhile.html',
                     slug: 'is-async-worthwhile'
+                },
+                {
+                    title: 'Why use an ORM?',
+                    src: 'why_use_an_orm.html',
+                    slug: 'why-use-an-orm'
                 }
             ]
         }
