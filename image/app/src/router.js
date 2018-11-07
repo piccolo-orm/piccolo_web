ce0dd2d4276d524c88e49cda27362e97f01cbb7f
Daniel Townsend <dan@dantownsend.co.uk>
2018-11-07 22:23:15 +0000

    removing pre-render plugin - pain to use in docker container because needs chrome

--- image/app/src/router.js
+++ image/app/src/router.js
@@ -23,14 +23,6 @@ export default new Router({
       name: 'blog_single',
       component: BlogSingle,
       props: true
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
