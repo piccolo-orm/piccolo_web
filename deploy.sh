a9d80b8a55e37391fd0651c341bcfdfd05da29ae
Daniel Townsend <dan@dantownsend.co.uk>
2019-01-02 22:14:11 +0000

    added google webmaster tools search console verification tag

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 set -e
 ./build-image.sh
 ./push-image.sh
-ssh root@213.32.70.59 'docker pull registry-1.dantownsend.co.uk:5000/piccolo_web:latest && cd /home/core/apps/piccolo && /opt/bin/docker-compose up -d && yes y | docker image prune'
+
+ssh root@piccolo-orm.com 'docker pull registry-1.dantownsend.co.uk:5000/piccolo_web:latest && cd /home/core/apps/piccolo && /opt/bin/docker-compose up -d && yes y | docker image prune'
