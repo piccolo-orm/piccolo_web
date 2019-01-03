52627d9409e4d017f1d9e2066b3caf0f0c3e35a9
Daniel Townsend <dan@dantownsend.co.uk>
2019-01-03 15:00:02 +0000

    making it public

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-set -e
-./build-image.sh
-./push-image.sh
-
-ssh root@piccolo-orm.com 'docker pull registry-1.dantownsend.co.uk:5000/piccolo_web:latest && cd /home/core/apps/piccolo && /opt/bin/docker-compose up -d && yes y | docker image prune'
