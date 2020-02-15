143c760c645a793d566b5c5cdd46beed9af10fd9
Daniel Townsend <dan@dantownsend.co.uk>
2020-02-15 10:11:22 +0000

    removed docker

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 The Piccolo ORM website, deployed here:
 
  * https://piccolo-orm.com
-
-## What kind of project is this?
-
-It's a Vue JS app, which is deployed within Docker.
--- build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-version=$(cat vars/version.txt)
-name=$(cat vars/name.txt)
-registry=$(cat vars/registry.txt)
-
-docker build -t $registry/$name:$version ./image
-docker tag $registry/$name:$version $registry/$name:latest
